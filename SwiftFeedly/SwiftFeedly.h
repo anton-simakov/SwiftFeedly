f141266b62a8c901c258fe1703b20271804b6a4d
Anton Simakov <simakov.anton@hotmail.com>
2020-04-30 13:38:39 +0300

    Add Xcode project

---
+++ SwiftFeedly/SwiftFeedly.h
@@ -0,0 +1,19 @@
+//
+//  SwiftFeedly.h
+//  SwiftFeedly
+//
+//  Created by Anton Simakov on 4/30/20.
+//  Copyright © 2020 Anton Simakov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftFeedly.
+FOUNDATION_EXPORT double SwiftFeedlyVersionNumber;
+
+//! Project version string for SwiftFeedly.
+FOUNDATION_EXPORT const unsigned char SwiftFeedlyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftFeedly/PublicHeader.h>
+
+
